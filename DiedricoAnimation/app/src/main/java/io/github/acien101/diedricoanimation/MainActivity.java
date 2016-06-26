de2de32a4db3daab8122dfbfaf0d83821da66049
Fran Acien <amil101ftw@gmail.com>
2016-06-26 03:20:59 +0200

    adding names for toolbar

--- DiedricoAnimation/app/src/main/java/io/github/acien101/diedricoanimation/MainActivity.java
+++ DiedricoAnimation/app/src/main/java/io/github/acien101/diedricoanimation/MainActivity.java
@@ -162,20 +162,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
+        if (id == R.id.welcome) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.edges) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.pointProjection) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.lineProjection) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.typeOflines) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.typeOfPlanes) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
